14a666f1660e0cc5078bed468a7069875603105f
JRixGer <jrixcgeromo@gmail.com>
2018-09-30 13:29:47 +0800

    new updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,8 +13,6 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
 
-    use Illuminate\Support\Facades\Schema;
-    
     public function boot()
     {
         Schema::defaultStringLength(191);
