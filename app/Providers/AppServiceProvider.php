86f069bafac6ea190d8486bb54edace8fe688d6e
JRixGer <jrixcgeromo@gmail.com>
2018-09-30 13:29:01 +0800

    new updates #2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+
+    use Illuminate\Support\Facades\Schema;
+    
     public function boot()
     {
         Schema::defaultStringLength(191);
